c82f24bbc824247fcef8761e95089d719e1fdf77
Avi Warner <aviwarner@gmail.com>
2018-01-11 13:07:10 -0800

    Menu assignment work

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_number
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -54,6 +59,23 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_number
+    print "Enter an entry number (or 0 to return to main menu): "
+    selection = gets.chomp.to_i - 1
+    if address_book.entries[selection] && selection >= 0
+      system "clear"
+      puts address_book.entries[selection].to_s
+      view_entry_number
+    elsif selection == -1
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "There's no entry with that number."
+      view_entry_number
+    end
   end
 
   def entry_submenu(entry)
