b76524c0b85c20deaee2decc09c3be5465dd26b7
Avi Warner <aviwarner@gmail.com>
2018-01-12 14:58:52 -0800

    Menus continued assignment work

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Delete all entries"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system "clear"
+        nuke_entries
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -179,6 +184,18 @@ class MenuController
     rescue
       puts "#{file_name} is not a valid CSV file, please enter the name of a Valid CSV."
       read_csv
+    end
+  end
+
+  def nuke_entries
+    puts "Are you sure? You can't undelete! (y/n): "
+    selection = gets.chomp
+    case selection
+      when "y"
+        address_book.entries.clear
+        puts "All entries deleted!"
+      else
+        puts "I knew you didn't want to do that."
     end
   end
 end
